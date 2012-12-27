f046ecca2a0a92bfb5167a6a578f188dc11d19d3
James Fisher <jameshfisher@gmail.com>
2012-12-27 11:40:36 +0000

    Update yesod version; use simple yesod init; configure keter

--- Import.hs
+++ Import.hs
@@ -10,10 +10,10 @@ import           Control.Applicative  as Import (pure, (<$>), (<*>))
 import           Data.Text            as Import (Text)
 
 import           Foundation           as Import
-import           Model                as Import
 import           Settings             as Import
 import           Settings.Development as Import
 import           Settings.StaticFiles as Import
+import           Data.Aeson           as Import (toJSON)
 
 #if __GLASGOW_HASKELL__ >= 704
 import           Data.Monoid          as Import
