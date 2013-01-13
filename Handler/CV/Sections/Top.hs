99797fa509841a9e8a0185e11014103671cac38e
James Fisher <jameshfisher@gmail.com>
2013-01-13 23:59:31 +0000

    WIP

--- Handler/CV/Sections/Top.hs
+++ Handler/CV/Sections/Top.hs
@@ -1,9 +1,27 @@
 module Handler.CV.Sections.Top (topSection) where
 
+import Import
+
 import Prelude
 import Text.XML.HXT.Core (XmlTree, ArrowXml, sattr, txt, mkelem)
 import Handler.CV.Common (h2, ul, strong, link, cls, section, attrVal, email, obscure, csv, item, mk, trs, prop, htmlSpan, division)
 import Handler.CV.Links (mkLink, Link(LinkMeLinkedIn, LinkMeBitbucket, LinkMeGitHub, LinkMeReddit))
+
+-- WIP
+topSection' = [hamlet|
+<section itemscope="itemscope" itemtype="http://data-vocabulary.org/Person">
+  <span style="display: none;" itemprop="name">James Harrison Fisher
+  <div .row>
+    <div .span8>
+      <table .table .table-condensed .table-quiet>
+        <tbody>
+          <tr>
+            <th>Birth date
+            <td>October 1987
+          <tr>
+            <th>Address
+            <td>
+|]
 
 topSection :: (ArrowXml a) => a n XmlTree
 topSection = section (item "http://data-vocabulary.org/Person")
